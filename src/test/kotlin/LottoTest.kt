e67d906116bc064005b93c0feb6d640d89422b47
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-09 13:00:56 +0900

    feat(Lotto): 로또 번호 자동 발행

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,5 +1,7 @@
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -35,24 +37,53 @@ class LottoTest {
             Lotto.from(*numbers)
         }
     }
+
+    @Test
+    fun `로또를 생성하면 6개의 로또 번호가 발행된다`() {
+        val lotto = Lotto.create()
+        val actual = lotto.size
+        val expected = 6
+
+        assertThat(actual).isEqualTo(expected)
+    }
 }
 
 data class Lotto private constructor(
     private val lottoNumbers: Set<LottoNumber>
 ) {
     init {
-        require(lottoNumbers.size == 6) { "로또는 6개의 로또 번호를 가져야합니다." }
+        require(lottoNumbers.size == NUMBER_OF_LOTTO_NUMBERS) { "로또는 6개의 로또 번호를 가져야합니다." }
     }
 
     val size get() = lottoNumbers.size
 
     companion object {
+        private const val LOTTO_START_NUMBER = 1
+        private const val LOTTO_END_NUMBER = 45
+        private const val NUMBER_OF_LOTTO_NUMBERS = 6
+
         fun from(vararg number: Int): Lotto {
             val lottoNumbers = number.map { LottoNumber(it) }
             if (lottoNumbers.size != lottoNumbers.toSet().size) {
                 throw IllegalArgumentException("로또 번호는 중복되면 안됩니다.")
             }
             return Lotto(lottoNumbers.toSet())
+        }
+
+        fun from(numbers: List<Int>): Lotto {
+            val lottoNumbers = numbers.map { LottoNumber(it) }
+            if (lottoNumbers.size != lottoNumbers.toSet().size) {
+                throw IllegalArgumentException("로또 번호는 중복되면 안됩니다.")
+            }
+            return Lotto(lottoNumbers.toSet())
+        }
+
+        fun create(): Lotto {
+            val lottoNumbers =
+                (LOTTO_START_NUMBER..LOTTO_END_NUMBER)
+                    .shuffled()
+                    .take(NUMBER_OF_LOTTO_NUMBERS)
+            return from(lottoNumbers)
         }
     }
 }
