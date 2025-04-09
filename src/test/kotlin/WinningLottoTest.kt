026db578926a396a8a8cc3a7b4bc56f40834f819
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-09 22:10:37 +0900

    feat(WinningLottoNumber): factory 메서드 추가

--- src/test/kotlin/WinningLottoTest.kt
+++ src/test/kotlin/WinningLottoTest.kt
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.assertDoesNotThrow
 class WinningLottoTest {
     @Test
     fun `로또와 보너스 번호를 가진다`() {
-        val lotto = Lotto.from(1, 2, 3, 4, 5, 6)
-        val bonusNumber = LottoNumber.from(7)
-
-        assertDoesNotThrow { WinningLotto(lotto, bonusNumber) }
+        assertDoesNotThrow { WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7) }
     }
+
+
 }
 
-class WinningLotto(
+class WinningLotto private constructor(
     private val lotto: Lotto,
     private val bonusNumber: LottoNumber,
 ) {
-
+    companion object {
+        fun of(vararg lottoNumber: Int, number: Int): WinningLotto {
+            val lotto = Lotto.from(*lottoNumber)
+            val bonusNumber = LottoNumber.from(number)
+            return WinningLotto(lotto, bonusNumber)
+        }
+    }
 }
