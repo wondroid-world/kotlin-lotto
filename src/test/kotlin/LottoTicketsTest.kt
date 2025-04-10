6862a3afc9e5fb8b3d2a069ee55a4edef7bcc74b
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-10 12:25:27 +0900

    feat(LottoTickets): 총 당첨 등수를 계산하는 기능 구현

--- src/test/kotlin/LottoTicketsTest.kt
+++ src/test/kotlin/LottoTicketsTest.kt
@@ -1,17 +1,43 @@
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class LottoTicketsTest {
     @Test
     fun `로또 여러장을 가지고 있는 LottoTickets가 있다`() {
         val lottos = listOf<Lotto>(
-            Lotto.from(1,2,3,4,5,6),
-            Lotto.from(1,2,3,4,5,7),
-            Lotto.from(1,2,3,4,5,8),
+            Lotto.from(1, 2, 3, 4, 5, 6),
+            Lotto.from(1, 2, 3, 4, 5, 7),
+            Lotto.from(1, 2, 3, 4, 5, 8),
         )
+        assertDoesNotThrow {
+            LottoTickets(lottos)
+        }
+    }
+
+    @Test
+    fun `총 당첨 등수를 계산할 수 있다`() {
+        val lottos = listOf<Lotto>(
+            Lotto.from(1, 2, 3, 4, 5, 6),
+            Lotto.from(1, 2, 3, 4, 5, 7),
+            Lotto.from(1, 2, 3, 4, 5, 8),
+        )
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
         val lottoTickets = LottoTickets(lottos)
+
+        val actual = lottoTickets.calculateRank(winningLotto)
+
+        val expected = listOf<Rank>(Rank.FIRST, Rank.SECOND, Rank.THIRD)
+
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 }
 
 class LottoTickets(
-    val lottos: List<Lotto>,
-)
+    private val lottos: List<Lotto>,
+) {
+    fun calculateRank(winningLotto: WinningLotto): List<Rank> {
+        val ranks = lottos.map { lotto: Lotto -> winningLotto.rank(lotto) }
+        return ranks
+    }
+}
