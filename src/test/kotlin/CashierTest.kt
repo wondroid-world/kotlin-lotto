24633ceb351e04b7b6c487d652b4b1328f327182
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-10 17:31:16 +0900

    feat(Cashier): 로또 구입 개수 반환
    - class 명 변환(Amount -> Cashier)

--- src/test/kotlin/CashierTest.kt
+++ src/test/kotlin/CashierTest.kt
@@ -1,16 +1,15 @@
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
-class AmountTest {
+class CashierTest {
     @ParameterizedTest
     @ValueSource(ints = [-1000, -500])
     fun `구입 금액은 0보다 커야합니다`(money: Int) {
         assertThrows<IllegalArgumentException> {
-            Amount(money)
+            Cashier(money)
         }
     }
 
@@ -18,32 +17,44 @@ class AmountTest {
     @ValueSource(ints = [0, 999])
     fun `로또 최소 구매 단위는 1000원이다`(money: Int) {
         assertThrows<IllegalArgumentException> {
-            Amount(money)
+            Cashier(money)
         }
     }
 
     @ParameterizedTest
     @CsvSource(value = ["1500, 500", "14000, 0"])
     fun `잔돈을 거슬러 준다`(money: Int, excepted: Int) {
-        val amount = Amount(money)
+        val amount = Cashier(money)
 
         val actual = amount.charge()
 
         Assertions.assertThat(actual).isEqualTo(excepted)
     }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1500, 1", "14000, 14"])
+    fun `로또 구입 가능 갯수를 알수 있다`(money: Int, excepted: Int) {
+        val amount = Cashier(money)
+
+        val actual = amount.lottoCount
+
+        Assertions.assertThat(actual).isEqualTo(excepted)
+    }
 }
 
-@JvmInline
-value class Amount(
+class Cashier(
     private val money: Int,
 ) {
+    val lottoCount: Int
+        get() = money / 1000
+
     init {
         require(money >= 0) { "구입 금액은 0보다 커야합니다." }
         require(money > 1000) {"로또 최소 구매 단위는 1000원입니다."}
     }
 
     fun charge(): Int {
-        val charge = money - (money / 1000) * 1000
+        val charge = money - lottoCount * 1000
         return charge
     }
 }
