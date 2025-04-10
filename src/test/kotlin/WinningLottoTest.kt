29b2ffa4a4b41b3dd2ebcf68b2a77368bacb952b
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-10 11:52:08 +0900

    feat(WinningLotto): 각각의 로또 번호가 몇 등인지 계산

--- src/test/kotlin/WinningLottoTest.kt
+++ src/test/kotlin/WinningLottoTest.kt
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
@@ -7,13 +8,89 @@ class WinningLottoTest {
         assertDoesNotThrow { WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7) }
     }
 
+    @Test
+    fun `로또의 당첨 1등를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 3, 4, 5, 6)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
 
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.FIRST
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또의 당첨 2등를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 3, 4, 5, 7)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
+
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.SECOND
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또의 당첨 3등를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 3, 4, 5, 8)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
+
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.THIRD
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또의 당첨 4등를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 3, 4, 8, 9)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
+
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.FOURTH
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또의 당첨 5등를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 3, 8, 9, 10)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
+
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.FIFTH
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또의 당첨 꽝를 계산한다`() {
+        val lotto = Lotto.from(1, 2, 8, 9, 10, 11)
+        val winningLotto = WinningLotto.of(1, 2, 3, 4, 5, 6, number = 7)
+
+        val actual = winningLotto.rank(lotto)
+
+        val expected = Rank.MISS
+
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
 }
 
 class WinningLotto private constructor(
     private val lotto: Lotto,
     private val bonusNumber: LottoNumber,
 ) {
+    fun rank(lotto: Lotto): Rank {
+        val countOfMatch: Int = lotto.count(this.lotto)
+        val matchBonus: Boolean = lotto.contanins(bonusNumber)
+        return Rank.valueOf(countOfMatch, matchBonus)
+    }
+
     companion object {
         fun of(vararg lottoNumber: Int, number: Int): WinningLotto {
             val lotto = Lotto.from(*lottoNumber)
