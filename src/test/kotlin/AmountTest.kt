3f2e9949ae4503333eca61d3510cb709155bbf51
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-10 17:27:40 +0900

    feat(Amount): 잔돈 반환하는 기능 구현

--- src/test/kotlin/AmountTest.kt
+++ src/test/kotlin/AmountTest.kt
@@ -1,6 +1,8 @@
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class AmountTest {
@@ -19,6 +21,16 @@ class AmountTest {
             Amount(money)
         }
     }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1500, 500", "14000, 0"])
+    fun `잔돈을 거슬러 준다`(money: Int, excepted: Int) {
+        val amount = Amount(money)
+
+        val actual = amount.charge()
+
+        Assertions.assertThat(actual).isEqualTo(excepted)
+    }
 }
 
 @JvmInline
@@ -28,5 +40,10 @@ value class Amount(
     init {
         require(money >= 0) { "구입 금액은 0보다 커야합니다." }
         require(money > 1000) {"로또 최소 구매 단위는 1000원입니다."}
+    }
+
+    fun charge(): Int {
+        val charge = money - (money / 1000) * 1000
+        return charge
     }
 }
