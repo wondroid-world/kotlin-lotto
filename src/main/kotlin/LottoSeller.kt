fc58cfdf30d2f213df9fdbc526e83e0eb59ebfce
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-10 20:47:33 +0900

    feat(LottoSeller): 구매 금액에 따른 잔돈 반환하는 로직 및 구매 금액에 따른 로또 장수 반환 로직 구현

--- src/main/kotlin/LottoSeller.kt
+++ src/main/kotlin/LottoSeller.kt
@@ -1,6 +1,11 @@
 class LottoSeller {
     fun getCountOfLotto(money: Money): Int = money / PRICE_LOTTO
 
+    fun getCharge(money: Money): Money {
+        val charge = money % PRICE_LOTTO
+        return charge
+    }
+
     companion object {
         private val PRICE_LOTTO: Money = Money(1_000)
     }
--- src/test/kotlin/LottoSellerTest.kt
+++ src/test/kotlin/LottoSellerTest.kt
@@ -21,5 +21,23 @@ class LottoSellerTest {
 
         assertThat(actual).isEqualTo(excepted)
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        "15_600, 600",
+        "10_000, 0",
+        "1_000, 0",
+        "800, 800"
+    )
+    fun `구입 금액에 따라 잔돈을 반환한다`(value: Int, expected: Int) {
+        // given
+        val seller = LottoSeller()
+        val money = Money(value)
+
+        // when, then
+        val actual: Money = seller.getCharge(money)
+
+        assertThat(actual).isEqualTo(Money(expected))
+    }
 }
 
