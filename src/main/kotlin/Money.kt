e4d64ea78babde8357018b64a711e2a2859bedf8
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-10 19:13:18 +0900

    feat(Money): Money 값 객체의 연산(+, -, /) 로직 구현

---
+++ src/main/kotlin/Money.kt
@@ -0,0 +1,17 @@
+@JvmInline
+value class Money(
+    private val value: Int,
+) {
+    init {
+        require(value >= 0) { "돈은 0원이상이여야합니다." }
+    }
+
+    operator fun minus(other: Money): Money = Money(this.value - other.value)
+
+    operator fun plus(other: Money): Money = Money(this.value + other.value)
+
+    operator fun div(other: Money): Int {
+        require(other.value > 0) { "0원은 나눌 수 없습니다." }
+        return this.value / other.value
+    }
+}
---
+++ src/test/kotlin/MoneyTest.kt
@@ -0,0 +1,66 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class MoneyTest {
+    @Test
+    fun `14_000원 빼기 4_000원은 10_000원이다`() {
+        // given
+        val largeMoney = Money(14_000)
+        val smallMoney = Money(4_000)
+
+        // when, then
+        val excepted = Money(10_000)
+        val actual = largeMoney - smallMoney
+
+        assertThat(actual).isEqualTo(excepted)
+    }
+
+    @Test
+    fun `4_000원 빼기 14_000원은 에러를 반환한다`() {
+        // given
+        val largeMoney = Money(14_000)
+        val smallMoney = Money(4_000)
+
+        // when, then
+        assertThrows<IllegalArgumentException> { smallMoney - largeMoney }
+    }
+
+    @Test
+    fun `4_000원 더하기 10_000원는 14_000원이다`() {
+        // given
+        val largeMoney = Money(10_000)
+        val smallMoney = Money(4_000)
+
+        // when, then
+        val excepted = Money(14_000)
+        val actual = largeMoney + smallMoney
+
+        assertThat(actual).isEqualTo(excepted)
+    }
+
+    @Test
+    fun `14_000원 나누기 1_000원는 14이다`() {
+        // given
+        val largeMoney = Money(14_000)
+        val smallMoney = Money(1_000)
+
+        // when, then
+        val excepted = 14
+        val actual = largeMoney / smallMoney
+
+        assertThat(actual).isEqualTo(excepted)
+    }
+
+    @Test
+    fun `14_000원 나누기 0원는 에러를 반환한다`() {
+        // given
+        val largeMoney = Money(14_000)
+        val smallMoney = Money(0)
+
+        // when, then
+        assertThrows<IllegalArgumentException> { largeMoney / smallMoney }
+    }
+}
+
