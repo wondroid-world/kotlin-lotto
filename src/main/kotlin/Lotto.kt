e33d7fb234d9ddf68434a0c5b49396d4e474e983
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-11 17:40:06 +0900

    feat(Lotto): 로또 번호 일치 개수를 계산하는 기능 추가

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -1,14 +1,17 @@
 data class Lotto(
-    private val numbers: List<LottoNumber>
+    private val numbers: Set<LottoNumber>
 ) {
     init {
+        require(numbers.size == 6)
+    }
+
+    constructor(nums: List<LottoNumber>) : this(nums.toSet()) {
         require(numbers.size == numbers.toSet().size)
     }
 
-    companion object {
-        operator fun invoke(vararg lottoNumbers: Int): Lotto {
-            val numbers = lottoNumbers.map{ LottoNumber(it)}
-            return Lotto(numbers)
-        }
-    }
+    constructor(vararg nums: Int) : this(nums.map { LottoNumber(it) })
+
+    fun matchCount(other: Lotto): Int = numbers.count { num -> other.contains(num) }
+
+    fun contains(num: LottoNumber): Boolean = num in numbers
 }
--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -11,6 +12,43 @@ class LottoTest {
     @Test
     fun `로또에서 동일한 숫자가 올 시, 에러를 반환한다`() {
         assertThrows<IllegalArgumentException> { Lotto(1, 2, 3, 4, 5, 5) }
+    }
+
+    @Test
+    fun `로또는 특정 번호를 포함을 하면, true를 반환한다`() {
+        // given
+        val lotto = Lotto(1, 2, 3, 4, 5, 6)
+        val num = LottoNumber(6)
+
+        // when, then
+        val actual = lotto.contains(num)
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `로또는 특정 번호를 포함하지 않으면, false를 반환한다`() {
+        // given
+        val lotto = Lotto(1, 2, 3, 4, 5, 6)
+        val num = LottoNumber(7)
+
+        // when, then
+        val actual = lotto.contains(num)
+
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `로또는 다른 로또 번호들과 매칭되는 개수를 반환한다`() {
+        // given
+        val lotto = Lotto(1, 2, 3, 4, 5, 6)
+        val other = Lotto(1, 2, 3, 4, 5, 6)
+
+        // when, then
+        val excepted: Int = 6
+        val actual = lotto.matchCount(other)
+
+        assertThat(actual).isEqualTo(excepted)
     }
 }
 
