54451b4a5adbbb0ee7c63dc8ed68ade9c1c31a41
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-15 14:56:52 +0900

    feat(BonusNumber): 보너스 번호 정수 생성 경로 추가

--- src/main/kotlin/domain/BonusNumber.kt
+++ src/main/kotlin/domain/BonusNumber.kt
@@ -3,7 +3,12 @@ package domain
 data class BonusNumber(
     private val number: LottoNumber
 ) {
+
     fun contains(lotto: Lotto): Boolean = lotto.contains(number)
 
     fun isSame(number: LottoNumber): Boolean = this.number == number
+
+    companion object {
+        fun from(num: Int): BonusNumber = BonusNumber(LottoNumber(num))
+    }
 }
